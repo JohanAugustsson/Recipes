4552af22440d997a0031c27a3786a867dd68c453
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-04-13 16:50:14 +0200

    added fontawesome and allrecipe

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
+    <i class="fas fa-heart"></i>
 
     <landing-page/>
-    
+    <all-recipes/>
+
     <one-recipe :selected="selectedRecipe"/> <!--Visar ett recept -->
   </div>
 </template>
 
 <script>
+  import "../../static/fontAwsome/fontawesome-all.js";
   import landingPage from './landingpage/landingpage.vue'
   import oneRecipe from './oneRecipe/oneRecipe.vue';
+  import allRecipes from './allRecepies/allRecepies.vue'
   const recipesObject = require('../mocks/recipes.json');  // databas: json object med alla recept som vi har.
+
   console.log(recipesObject);
 
   export default {
@@ -24,6 +29,7 @@
     },
     components: {
       "one-recipe" : oneRecipe,
+      "all-recipes": allRecipes,
       "landing-page": landingPage
     }
   }
--- src/components/allRecepies/allRecepies.vue
+++ src/components/allRecepies/allRecepies.vue
@@ -1,5 +1,23 @@
 <template lang="html">
 
+  <div  class="recipeCard">
+    <div class="recipeImg-hoder">
+      <img class="recipeImg" src="https://aomeassetsprod.blob.core.windows.net/editorial/import/images/2011-11-09/mandel-och-majspannkakor.jpg" alt="bild">
+
+    </div>
+    <div class="recipeInfo">
+      <h3>tilel på recept</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.
+       </p>
+       <div class="ingrediens-pickMe">
+         <p> {{antalIngred}} ingredienser</p>
+         <a href=""><i class="fas fa-heart"></i> välj mig</a>
+       </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -7,5 +25,32 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped lang="css">
+
+.recipeImg-hoder{
+  margin-right: 1rem;
+  width: 200px;
+}
+.recipeImg{
+  margin-top: 1rem;
+  width: 100%;
+}
+.recipeCard{
+  width: 40%;
+  min-width: 320px;
+
+  background-color: powderblue;
+  display: flex;
+}
+.recipeInfo{
+  width: 100%;
+  flex-direction: column;
+
+}
+.ingrediens-pickMe{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+}
 </style>
